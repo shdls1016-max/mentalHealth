65b852d449b3cb380204e7f0460da96c152f5f05
shdls1016 <shdls1016@gmail.com>
2026-07-04 20:18:29 +0900

    서브4-1 반응형 360까지

--- js/sub.js
+++ js/sub.js
@@ -31,27 +31,66 @@ if(breadgnb && breadlnb){
     
 }
 
+const sub1_2 = document.body.classList.contains('sub1-2'); 
+const sub4_1 = document.body.classList.contains('sub4-1'); 
+
 
 /* 서브1-2 둘러보기 기능 */
-const selectedImgTag = document.querySelector('.sub1-2cont--big').children[0];
-const selectedImg = document.querySelector('.sub1-2cont--big').children[1];
-const forselectList = document.querySelectorAll('.sub1-2cont--smalls > li');
+if(sub1_2){
 
-forselectList.forEach((liImg)=>{
-    liImg.addEventListener('click', (e)=>{
-        const forchangeTag= liImg.children[0].getAttribute('alt');
-        const forchangeSrc= liImg.children[0].getAttribute('src');
-        const changeTag = forchangeTag.slice(0,-5);
-        
-        forselectList.forEach((li)=>{
-            li.classList.remove('selected');
+    const selectedImgTag = document.querySelector('.sub1-2cont--big').children[0];
+    const selectedImg = document.querySelector('.sub1-2cont--big').children[1];
+    const forselectList = document.querySelectorAll('.sub1-2cont--smalls > li');
+
+    forselectList.forEach((liImg)=>{
+        liImg.addEventListener('click', (e)=>{
+            const forchangeTag= liImg.children[0].getAttribute('alt');
+            const forchangeSrc= liImg.children[0].getAttribute('src');
+            const changeTag = forchangeTag.slice(0,-5);
+            
+            forselectList.forEach((li)=>{
+                li.classList.remove('selected');
+            })
+
+            selectedImgTag.textContent = changeTag;
+            selectedImg.setAttribute('src', forchangeSrc);
+            liImg.classList.add('selected');
+
+            
         })
 
-        selectedImgTag.textContent = changeTag;
-        selectedImg.setAttribute('src', forchangeSrc);
-        liImg.classList.add('selected');
-
-        
     })
+}
 
-})
+
+
+/* 서브4-1 아코디언 기능 */
+if(sub4_1){
+    const faqQuest = document.querySelectorAll('.sub4-1cont--quest')
+    const faqAnswer = document.querySelectorAll('.sub4-1cont--answer')
+
+    if(faqQuest && faqAnswer){
+        faqQuest.forEach((quest, idx)=>{
+            quest.addEventListener('click', ()=>{
+                faqQuest.forEach(q=>{
+                    q.classList.remove('active');
+                    q.children[1].classList.remove('rotate180_effect');
+                    q.children[1].classList.add('rotate360_effect');
+                });
+
+                faqAnswer.forEach(a=>{
+                    a.classList.remove('active');
+                    a.classList.add('dn');
+                })
+
+                quest.classList.add('active');
+                quest.children[1].classList.add('rotate180_effect');
+                quest.children[1].classList.remove('rotate360_effect');
+
+                faqAnswer[idx].classList.add('active');
+                faqAnswer[idx].classList.remove('dn');
+            });
+        });
+    }
+
+}
